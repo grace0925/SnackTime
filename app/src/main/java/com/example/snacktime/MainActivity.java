fb90ea79f7358b9e3e3f0383816d6cb8a30dcdbd
graceliu0925 <grace@emailgistics.com>
2019-06-05 10:46:57 -0400

    😍 Fix png file name, buttons

--- app/src/main/java/com/example/snacktime/MainActivity.java
+++ app/src/main/java/com/example/snacktime/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.snacktime;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button signUpBtn, loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signUpBtn = (Button) findViewById(R.id.signup_btn);
+        loginBtn = (Button) findViewById(R.id.login_btn);
     }
 
 }
