5bf71994acf9324341713da1b143d020ab0b5790
GraceLiu <graceliu0925@gmail.com>
2019-07-04 09:43:54 -0400

    💩 Signup activity on create

--- app/src/main/java/com/example/snacktime/SignupActivity.java
+++ app/src/main/java/com/example/snacktime/SignupActivity.java
@@ -2,12 +2,25 @@ package com.example.snacktime;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignupActivity extends AppCompatActivity {
+
+    private Button SignupButton;
+    private EditText InputUsername, InputFirstname, InputLastname, Inputemail, Inputpw, InputConfirmPw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        SignupButton = (Button) findViewById(R.id.signup);
+        InputUsername = (EditText) findViewById(R.id.username_edit);
+        InputFirstname = (EditText) findViewById(R.id.firstname_edit);
+        InputLastname =(EditText) findViewById(R.id.lastname_edit);
+        Inputemail = (EditText) findViewById(R.id.email_signup);
+        Inputpw = (EditText) findViewById(R.id.password_signup);
+        InputConfirmPw = (EditText) findViewById(R.id.confirm_password_signup);
     }
 }
