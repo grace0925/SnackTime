53c62a57720ee25abc1ef5cb39896d4c1062a14b
GraceLiu <graceliu0925@gmail.com>
2019-07-04 16:42:51 -0400

    💕 Login activity onCreate

--- app/src/main/java/com/example/snacktime/LoginActivity.java
+++ app/src/main/java/com/example/snacktime/LoginActivity.java
@@ -1,16 +1,38 @@
 package com.example.snacktime;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText inputUsername, inputPassword;
+    private Button loginBtn;
+    private ProgressDialog loading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        setTheme(R.style.LightTheme);
-        this.setContentView(R.layout.activity_login);
+        inputUsername = (EditText) findViewById(R.id.username_edit);
+        inputPassword = (EditText) findViewById(R.id.password_edit);
+        loginBtn = (Button) findViewById(R.id.login);
+        loading = new ProgressDialog(this);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+            }
+        });
+    }
+
+    private void login() {
+        
     }
 }
