1a52843030e6d0b999ddf88444d2c774f67543ed
GraceLiu <graceliu0925@gmail.com>
2019-07-04 10:09:05 -0400

    🐷 CreateAccount() signup activity

--- app/src/main/java/com/example/snacktime/SignupActivity.java
+++ app/src/main/java/com/example/snacktime/SignupActivity.java
@@ -1,9 +1,13 @@
 package com.example.snacktime;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -22,5 +26,29 @@ public class SignupActivity extends AppCompatActivity {
         Inputemail = (EditText) findViewById(R.id.email_signup);
         Inputpw = (EditText) findViewById(R.id.password_signup);
         InputConfirmPw = (EditText) findViewById(R.id.confirm_password_signup);
+
+        SignupButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                CreateAccount();
+            }
+        });
+    }
+
+    public void CreateAccount() {
+        String username = InputUsername.getText().toString();
+        String firstname = InputFirstname.getText().toString();
+        String lastname = InputLastname.getText().toString();
+        String email = Inputemail.getText().toString();
+        String password = Inputpw.getText().toString();
+        String confirmPw = InputConfirmPw.getText().toString();
+
+        checkEmptyEditText(username);
+    }
+
+    public void checkEmptyEditText(String str) {
+        if(TextUtils.isEmpty(str)) {
+            Toast.makeText(this, str.substring(0, 1).toUpperCase() + str.substring(1) + " can't be empty", Toast.LENGTH_SHORT).show();
+        }
     }
 }
