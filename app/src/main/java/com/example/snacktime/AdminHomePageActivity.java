c8b5ffb7f9c897ecc3a9f80e3ede8d42fd5a2047
GraceLiu <graceliu0925@gmail.com>
2019-07-15 21:54:44 -0400

    🔮 Clean up code

--- app/src/main/java/com/example/snacktime/AdminHomePageActivity.java
+++ app/src/main/java/com/example/snacktime/AdminHomePageActivity.java
@@ -5,6 +5,15 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
+
+//todo: dashboard
+//log in with other accounts
+//user: weather, search bar at top, recommended (sliding), little user icon, my profile page, food vlog, store list, rate food/comment, shopping cart, save store/order, speech
+//seller: should display distance, rating, price; add item, sales, take pictures(vision api?), receive order, confirm order, set open time
+//driver: receive notification, track location, timer
+//admin: view all stores(transaction history), put up promotions, blacklist user/seller/driver, view transaction history, notification(store requests)
+
+
 public class AdminHomePageActivity extends AppCompatActivity {
 
     @Override
